d9e5231a42ed1089b7efa0ce0eb7dea7acd296af
Dimitrios <hey@dnicolay.de>
2026-02-14 01:04:19 +0100

    Fix Tailwind config: add darkMode and pages path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     './app/**/*.{js,jsx,ts,tsx,mdx}',
     './pages/**/*.{js,jsx,ts,tsx,mdx}',
     './components/**/*.{js,jsx,ts,tsx,mdx}',
+    './contexts/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: { extend: {} },
   plugins: [
